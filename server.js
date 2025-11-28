c564d650d6b2733361b1283631000714cc243ffb
Sayali <vedpathaksayali10@gmail.com>
2025-11-28 12:24:16 +0530

    changes

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/register', (req, res) => {
+    const { name, email, age, address, phone } = req.body;
+    res.send(`Student Registered: ${name}, ${email}, ${age}, ${address}, ${phone}`);
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
